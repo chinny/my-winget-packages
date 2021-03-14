6b3e2b6abe5bae85179ab8c2370c3ebb802e483a
Jeffrey Chin <jeffreychin89@gmail.com>
2021-03-14 14:28:50 -0700

    init commit

---
+++ my-winget -gaming.ps1
@@ -0,0 +1,6 @@
+﻿winget install Valve.Steam  ; 
+winget install EpicGames.EpicGamesLauncher  ; 
+winget install Nvidia.GeForceExperience  ; 
+winget install ElectronicArts.Origin  ; 
+winget install Playnite.Playnite  ; 
+winget install Blizzard.BattleNet  ; 
